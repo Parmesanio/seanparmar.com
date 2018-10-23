705a5b38178619eaccf9d55cb16c79a679c621cf
Parmesanio <sean_parmar@yahoo.com>
2018-10-22 22:28:16 -0700

    Added Header component and reset.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -12,21 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <FontAwesomeIcon icon={['fab', 'app-store']} />
+          <Header />
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Header = () => {
+    return ( 
+        <header>
+            <nav>
+                <a href="#"><h1>Sean Parmar</h1></a>
+                <a href="#">Home</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Contact</a>
+                <a href="#" target="_blank" rel="noopener noreferrer">Blog</a>
+                <a href="#" target="_blank" rel="noopener noreferrer">Resume</a>
+            </nav>
+            <div className="socials">
+                <a href="https://www.linkedin.com/in/sean-parmar-996132135/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></a>
+                <a href="https://github.com/Parmesanio" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+                <a href="https://codepen.io/Parmesanio/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'codepen']} /></a>
+                <a href="https://itunes.apple.com/us/developer/sean-parmar/id1356437548?mt=8" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'app-store']} /></a>
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;
