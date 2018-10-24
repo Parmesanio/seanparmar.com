11524e54a170c4c850e177edc534f7d94be28736
Parmesanio <sean_parmar@yahoo.com>
2018-10-23 20:24:01 -0700

    Added Showcase, Portfolio, Skills, and Contact Components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './header.scss';
 
 const Header = () => {
     return ( 
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './landing.scss';
+import Showcase from '../Showcase/Showcase';
+import Skills from '../Skills/Skills';
+import Portfolio from '../Portfolio/Portfolio';
+import Contact from '../Contact/Contact';
 
 const Landing = () => {
-    return ( <div>Landing</div> );
+    return ( 
+    <div>
+        <Showcase />
+        <Skills />
+        <Portfolio />
+        <Contact />
+    </div> );
 }
  
 export default Landing;
