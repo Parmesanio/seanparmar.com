cdb4598ec9770539464acca4a39a455176730078
Parmesanio <sean_parmar@yahoo.com>
2018-11-15 12:20:49 -0700

    Added Create Admin unit test

--- server/lib/Admin.test.js
+++ server/lib/Admin.test.js
@@ -15,4 +15,21 @@ describe('Unit Tests', () => {
             return AdminData.getAdmin(fakeDB)
         })
     })
+    describe('Create admin', () => {
+        it('Should send admin data to the db', () => {
+            const fakeDB = {
+                query: sinon.mock().withArgs(
+                    sinon.match.string
+                )
+            }
+            const admin = {
+                auth0_id: 'facebook|2364333683593240',
+                username: 'Sean Parmar',
+                email: 'sean.parmar@yahoo.com',
+                created_at: new Date(),
+                photos: 'https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2364333683593240&height=50&width=50&ext=1542583011&hash=AeTN0SqI2Za8SBuz'
+            }
+            return AdminData.createAdmin(fakeDB, admin)
+        })
+    })
 })
