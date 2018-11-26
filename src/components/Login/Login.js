b397a4360458e9180da40afdd8a099bcd332c170
Parmesanio <sean_parmar@yahoo.com>
2018-11-26 14:22:49 -0700

    redefined scope

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Login = (props) => {
     const login = () => {
         const redirectUri = encodeURIComponent(`${window.location.origin}/auth/callback`);
+        const scope = encodeURIComponent('openid profile email');
         window.location = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${process.env.REACT_APP_AUTH0_CLIENT_ID}&scope=${scope}&redirect_uri=${redirectUri}&response_type=code`;
       }
     return ( 
